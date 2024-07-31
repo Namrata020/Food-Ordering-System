f30333faa0015d47b945d3100da1a4cca23931fb
Namrata Pandey <71514235+Namrata020@users.noreply.github.com>
2024-07-31 13:11:51 +0530

    Delete Online-Food-Ordering directory

--- Online-Food-Ordering/src/main/java/com/app/dao/CartDao.java
+++
@@ -1,9 +0,0 @@
-package com.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.entities.Cart;
-
-public interface CartDao extends JpaRepository<Cart,Long>{
-
-}
--- Online-Food-Ordering/src/main/java/com/app/entities/USER_ROLE.java
+++
@@ -1,5 +0,0 @@
-package com.app.entities;
-
-public enum USER_ROLE {
-	ROLE_CUSTOMER,ROLE_RESTAURANT_OWNER,ROLE_ADMIN
-}
